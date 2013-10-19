d86380fb8c8fb900a0d5282db15250d50fcf1198
beono <ibeono@gmail.com>
2013-10-19 11:17:05 +0400

    upload to github

---
+++ src/beono.mastercomponent/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+    "VERSION" => "1.0.6",
+    "VERSION_DATE" => "2012-07-30 00:00:00"
+);
+?>
---
+++ src/beono.mastercomponent/updater.php
@@ -0,0 +1,3 @@
+<?php
+CopyDirFiles(dirname(__FILE__)."/install/wizards/beono/", $_SERVER["DOCUMENT_ROOT"]."/bitrix/wizards/beono/", true, true);
+?>
